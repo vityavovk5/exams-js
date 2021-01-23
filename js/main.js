b69f249753b6b5e08902e67e1dcbca83f4875d95
vityavovk5 <vityavovk55@gmail.com>
2021-01-24 01:48:42 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -49,4 +49,21 @@ window.onclick = function(event) {
    
 }
  
+//
  
+$(document).ready(function(){
+    $(".news-img").click(function() {
+        modal2.style.display = "block";
+    });
+});
+var modal2 = document.getElementById("myModal2");
+var span9 = document.getElementsByClassName("close9")[0];
+span9.onclick = function() {
+    modal2.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+   
+}
